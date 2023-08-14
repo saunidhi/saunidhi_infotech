3a0b58dd0b1cc605471e79855c5d3ce165bd0623
AbhishekSharma42 <89927608+AbhishekSharma42@users.noreply.github.com>
2023-08-14 09:46:07 +0530

    side bar add sub menu and add instagram link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,10 +11,10 @@ function Footer() {
                 </span>
                 <span className='uppercase text-xs flex flex-wrap sm:flex-nowrap justify-center items-center gap-4 md:mx-[40px]'>
                     <div className="icons flex my-1">
-                        <Link > <LiaInstagram size={30} target="_blank" /></Link>
+                        <Link to="https://www.instagram.com/saunidhi_infotech" target="_blank" > <LiaInstagram size={30} /></Link>
                         <Link to="https://twitter.com/saunidhi46598" target="_blank"> <LiaTwitter size={30} /></Link>
-                        <Link to="https://www.facebook.com/profile.php?id=100094149437188"> <LiaFacebook size={30} target="_blank" /></Link>
-                        <Link to="https://www.linkedin.com/in/saunidhi-infotech-34b07b282/"> <LiaLinkedinIn size={30} target="_blank" /></Link>
+                        <Link to="https://www.facebook.com/profile.php?id=100094149437188" target="_blank"> <LiaFacebook size={30}  /></Link>
+                        <Link to="https://www.linkedin.com/in/saunidhi-infotech-34b07b282" target="_blank" > <LiaLinkedinIn size={30} /></Link>
                         {/* <LiaYoutube size={30} /> */}
                     </div>
                 </span>
--- src/Components/NavBar/MobileNavBar.jsx
+++ src/Components/NavBar/MobileNavBar.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { FaWindowClose } from "react-icons/fa";
+import { FaWindowClose, FaAngleDown } from "react-icons/fa";
 import { Link, useLocation } from 'react-router-dom';
-
-
 
 function MobileNavBar(props) {
 
@@ -25,6 +23,13 @@ function MobileNavBar(props) {
         }
     }
 
+
+    // this method for only Bottom to top scrolling 
+    const GotoUp = () => {
+            props.Toggle();
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth", });
+    }
+
     return (
         <>
             <div className={`fixed h-[100%] w-[100%] bg-black/80 hover:scroll-auto z-50 `}>
@@ -35,20 +40,26 @@ function MobileNavBar(props) {
                     </header>
                     <div className="text-[10px] propertes uppercase flex flex-col font-semi items-center gap-2 my-4">
 
-                        <Link to="/" onClick={Goto} className="Services p-2 text-base w-[80%] text-white hover:bg-red-500 cursor-pointer hover:text-white rounded-2xl text-center">Services</Link>
+                        <div className="Services group relative menu-hover flex text-base w-[100%] text-white hover:bg-red-500 cursor-pointer hover:text-white gap-4 p-2">
+                            <div className="pl-14">Services</div>
+                            <FaAngleDown />
+                            <div className="invisible fixed z-50 flex flex-col bg-black w-[75%] text-white shadow-xl group-hover:visible px-8 mt-[35px] mx-[-3px]">
+                                <Link to="/" onClick={Goto} className=" block my-3 sm:text-[10px] md:text-base text-center hover:bg-red-500 cursor-pointer hover:text-white w-[100%]">Services</Link>
+                                <Link onClick={GotoUp} to="Healthcare" className=" block my-3 sm:text-[10px] md:text-base text-center hover:bg-red-500 cursor-pointer hover:text-white w-[100%]">HealthCare</Link >
+                            </div>
+                        </div>
 
-                        <Link onClick={GotoProjectIdea} className="Services p-2 text-base w-[80%] text-white hover:bg-red-500 cursor-pointer hover:text-white 
-                        rounded-2xl text-center">SOLUTION</Link>
+                        <Link onClick={GotoProjectIdea} className="Services p-2 text-base w-[100%] text-white hover:bg-red-500 cursor-pointer hover:text-white pl-16">SOLUTION</Link>
 
                         {/* this is disable for some time */}
-                        <Link to="" onClick={props.Toggle} className="hidden Services p-2 text-base w-[80%] text-white hover:bg-red-500 cursor-pointer hover:text-white rounded-2xl text-center">PORTFOLIO</Link>
+                        <Link to="" onClick={props.Toggle} className="hidden Services p-2 text-base w-[100%] text-white hover:bg-red-500 cursor-pointer hover:text-white pl-16">PORTFOLIO</Link>
 
-                        <Link to="about" onClick={props.Toggle} className="Services p-2 text-base w-[80%] text-white hover:bg-red-500 cursor-pointer hover:text-white rounded-2xl text-center">ABOUT US</Link>
+                        <Link to="about" onClick={props.Toggle} className="Services p-2 text-base w-[100%] text-white hover:bg-red-500 cursor-pointer hover:text-white pl-16">ABOUT US</Link>
 
                         {/* this is disable for some time */}
-                        <Link to="" className="hidden Services p-2 text-base w-[80%] text-white hover:bg-red-500 cursor-pointer hover:text-white rounded-2xl text-center">BLOG</Link>
+                        <Link to="" className="hidden Services p-2 text-base w-[100%] text-white hover:bg-red-500 cursor-pointer hover:text-white pl-16">BLOG</Link>
 
-                        <Link to="contact" onClick={props.Toggle} className="Services p-2 text-base w-[80%] text-white hover:bg-red-500 cursor-pointer hover:text-white rounded-2xl text-center">CONTACT US</Link>
+                        <Link to="contact" onClick={props.Toggle} className="Services p-2 text-base w-[100%] text-white hover:bg-red-500 cursor-pointer hover:text-white pl-16">CONTACT US</Link>
                     </div>
                 </div>
             </div>
