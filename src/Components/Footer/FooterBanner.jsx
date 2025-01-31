051261d49cc8a3215b128eff073c12b3c9309d61
saunidhi <saunidhiinfotech@gmail.com>
2025-01-31 12:36:38 +0530

    Update FooterBanner.jsx

--- src/Components/Footer/FooterBanner.jsx
+++ src/Components/Footer/FooterBanner.jsx
@@ -7,10 +7,10 @@ function FooterBanner() {
         <div className="text uppercase text-[30px] text-white ">INDIA</div>
         <div className="sm:flex sm:flex-row sm:w-[100%] sm:justify-center sm:gap-5 ">
           <div className="para text-[18px] text-white w-[20rem] text-center ">Vasant Kunj,<br /> New Delhi, <br /> Delhi - 110067<br />
-            <p >admin@saunidhiinfotech.com</p>
+            <p >saunidhiinfotech@gmail.com</p>
           </div>
           <div className="para text-[18px] text-white w-[20rem] text-center py-5 sm:py-0">Agrasen Marg, <br /> Sonkh, Mathura,<br /> U.P. - 281123<br />
-            <p>admin@saunidhiinfotech.com</p>
+            <p>saunidhiinfotech@gmail.com</p>
           </div>
         </div>
       </div>
