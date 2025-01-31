576954075684e64cd19a0f103b7111b5cc57d340
saunidhi <saunidhiinfotech@gmail.com>
2025-01-31 12:55:34 +0530

    Update MainfacilityCard.jsx

--- src/Components/Facility/MainfacilityCard.jsx
+++ src/Components/Facility/MainfacilityCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import FacilityCardFirst from './FacilityCardFirst'
-import EducationInfo from './EducationInfo'
+{/*import EducationInfo from './EducationInfo' */}
 
 function MainfacilityCard() {
     return (
